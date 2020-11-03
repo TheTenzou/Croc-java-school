48c7a91723eaa43f5f290f41e8c8215839a7ac27
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-03 18:27:56 +0300

    Add annotations check in hashmap

--- Lecture4/Task2/src/main/java/ru/croc/java/school/lecture4/task2/HashMap.java
+++ Lecture4/Task2/src/main/java/ru/croc/java/school/lecture4/task2/HashMap.java
@@ -1,8 +1,19 @@
 package ru.croc.java.school.lecture4.task2;
 
+import ru.croc.java.school.lecture4.task2.Annotations.MapKeyFail;
+import ru.croc.java.school.lecture4.task2.Annotations.MapValueFail;
+import ru.croc.java.school.lecture4.task2.Exceptions.PutMapKeyFailException;
+import ru.croc.java.school.lecture4.task2.Exceptions.PutMapValueFailException;
+
 public class HashMap<K, V> extends java.util.HashMap<K, V> {
     @Override
-    public V put (K key, V value) {
+    public V put (K key, V value) throws PutMapKeyFailException, PutMapValueFailException {
+        if (key.getClass().isAnnotationPresent(MapKeyFail.class)) {
+            throw new PutMapKeyFailException();
+        }
+        if (value.getClass().isAnnotationPresent(MapValueFail.class)) {
+            throw new PutMapValueFailException();
+        }
         return super.put(key, value);
     }
 }

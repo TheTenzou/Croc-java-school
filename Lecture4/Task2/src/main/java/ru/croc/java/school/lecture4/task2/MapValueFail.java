f36eaaff3d405655e56771403b82bcdf6836752e
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-03 16:35:49 +0300

    Lecture4 task2 init

---
+++ Lecture4/Task2/src/main/java/ru/croc/java/school/lecture4/task2/MapValueFail.java
@@ -0,0 +1,11 @@
+package ru.croc.java.school.lecture4.task2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface MapValueFail {
+}

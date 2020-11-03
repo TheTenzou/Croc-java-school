aae756ad57046209c1e8134110f496e3ec96d64f
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-03 16:51:36 +0300

    Add Annotations

--- Lecture4/Task2/src/main/java/ru/croc/java/school/lecture4/task2/Annotations/MapKeyFail.java
+++ Lecture4/Task2/src/main/java/ru/croc/java/school/lecture4/task2/Annotations/MapKeyFail.java
@@ -1,4 +1,4 @@
-package ru.croc.java.school.lecture4.task2;
+package ru.croc.java.school.lecture4.task2.Annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface MapValueFail {
+public @interface MapKeyFail {
 }

eec83a250e1725369e7fc8f1643799f18812639e
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-09 17:10:53 +0300

    Add DataSourceProvider

---
+++ Lecture5/Task/src/main/java/ru/TheTenzou/croc/java/school/Lecture5/Task/DataBase/DataSourceProvider.java
@@ -0,0 +1,66 @@
+package ru.TheTenzou.croc.java.school.Lecture5.Task.DataBase;
+
+import org.apache.derby.jdbc.EmbeddedDataSource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class DataSourceProvider {
+
+    /**
+     * DataSource.
+     */
+    private EmbeddedDataSource dataSource;
+
+    /**
+     * Параметры конфигурации из файла application.properties.
+     */
+    private Map<String, String> properties = new HashMap<>();
+
+    /**
+     * Конструктор.
+     *
+     * @throws IOException ошибка инициализации
+     */
+    public DataSourceProvider() throws IOException {
+        loadProperties();
+    }
+
+    /**
+     * Метод заагрузки настроек конфигурации в память.
+     *
+     * @throws IOException ошибка получения настороек
+     */
+    private void loadProperties() throws IOException {
+        Properties properties = new Properties();
+        try {
+            properties.load(
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties"));
+            for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+                this.properties.put((String) entry.getKey(), (String) entry.getValue());
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка во время закрузки настороек");
+            throw e;
+        }
+    }
+
+    /**
+     * Метод получения экзамляра Datasource'а.
+     *
+     * @return data source
+     */
+    public EmbeddedDataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new EmbeddedDataSource();
+            dataSource.setUser("");
+            dataSource.setPassword("");
+            dataSource.setDatabaseName(properties.get("databasename"));
+            dataSource.setCreateDatabase("create");
+        }
+
+        return dataSource;
+    }
+}

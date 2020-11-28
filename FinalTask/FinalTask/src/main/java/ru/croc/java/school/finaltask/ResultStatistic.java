3ae1017a3b8827e4adb7ce5a62f7ba5e19cc7c90
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-28 14:28:00 +0300

    Add resultStatistic

---
+++ FinalTask/FinalTask/src/main/java/ru/croc/java/school/finaltask/ResultStatistic.java
@@ -0,0 +1,49 @@
+package ru.croc.java.school.finaltask;
+
+import java.time.LocalDate;
+
+/**
+ * Статистика по корона фирусной инфекции за задынный промкжуток.
+ */
+public class ResultStatistic {
+
+    /**
+     * Начало временного промежутка.
+     */
+    private LocalDate startDate;
+
+    /**
+     * Конец временного промежутка.
+     */
+    private LocalDate endDate;
+
+    /**
+     * Соотношение между выписаными поциентами и новыми выявлеными случаями.
+     */
+    private double ratio;
+
+    /**
+     * Конструктор.
+     *
+     * @param startDate начало временного промежутка
+     * @param endDate   конец временного промежутка
+     * @param ratio     соотношение между выписаными поциентами и новыми выявлеными случаями
+     */
+    public ResultStatistic(LocalDate startDate, LocalDate endDate, double ratio) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.ratio = ratio;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public double getRatio() {
+        return ratio;
+    }
+}

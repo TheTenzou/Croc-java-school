fe5e34bc7749c02bbefebd4864bca48d8c4faf95
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-28 15:30:47 +0300

    Add type adapter and ordering

--- FinalTask/FinalTask/src/main/java/ru/croc/java/school/finaltask/xml/ResultStatistic.java
+++ FinalTask/FinalTask/src/main/java/ru/croc/java/school/finaltask/xml/ResultStatistic.java
@@ -1,25 +1,32 @@
 package ru.croc.java.school.finaltask.xml;
 
+import ru.croc.java.school.finaltask.xml.converter.DateAdapter;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.LocalDate;
 
 /**
  * Статистика по корона фирусной инфекции за задынный промкжуток.
  */
 @XmlRootElement(name = "statistic")
+@XmlType(propOrder = {"startDate", "endDate", "ratio"})
 public class ResultStatistic {
 
     /**
      * Начало временного промежутка.
      */
     @XmlElement(name = "start-date")
+    @XmlJavaTypeAdapter(DateAdapter.class)
     private LocalDate startDate;
 
     /**
      * Конец временного промежутка.
      */
     @XmlElement(name = "end-date")
+    @XmlJavaTypeAdapter(DateAdapter.class)
     private LocalDate endDate;
 
     /**
